aa280ae1a22810b560ae3b1d324727216165f4dc
jeremyandelin1-tech <jeremyandelin1@gmail.com>
2025-12-03 18:07:25 -0700

    sounds

--- ttt/js/script.js
+++ ttt/js/script.js
@@ -1,112 +1,94 @@
-//Ticked Taxed Toes Games
+// Ticked Taxed Toes Game
+
+// Game state
 var markers = ["X", "O"];
 var players = [];
-players[0] = prompt("Player 1");
-players[1] = prompt("Player 2");
 var totals = [];
 var winCodes = [7, 56, 73, 84, 146, 273, 292, 448];
-var gameOver;
-// player1 wins, player2 wins
-var wins = [0, 0]; 
+var gameOver = false;
+var wins = [0, 0];
+var ties = 0;
 var whoseTurn = 0;
+    players[0] = prompt("Player 1 name:") || "Player 1";
+    players[1] = prompt("Player 2 name:") || "Player 2";
+// Sounds
+var happySound = new Audio("sounds/happy.mp3");
+var sadSound = new Audio("sounds/sad.mp3");
 
+// Start a new game
+function startGame() {
+    // Prompt player names once per game start
 
-function startGame() 
-{
-	var counter = 1;
-	var innerDivs = "";
-	for (i = 1; i<=3; i++)
-	{
-	innerDivs += '<div id="row-' + i + '">';
-	for (j = 1; j <=3; j++)
-	{
-	innerDivs += '<div onclick="playGame(this,' + counter + ');"></div>';
-	counter *=2;
-	}
-	innerDivs += '</div>';
-}
-	document.getElementById("game-board").innerHTML = innerDivs;
-	totals = [0, 0];
-	gameOver = false;
-	document.getElementById("game-message").innerText = "It's " + players[whoseTurn] + "'s Turn";
-
-
-{
+    // Build board
     var counter = 1;
     var innerDivs = "";
-    for (i = 1; i <= 3; i++) {
+    for (var i = 1; i <= 3; i++) {
         innerDivs += '<div id="row-' + i + '">';
-        for (j = 1; j <= 3; j++) {
+        for (var j = 1; j <= 3; j++) {
             innerDivs += '<div onclick="playGame(this,' + counter + ');"></div>';
             counter *= 2;
         }
         innerDivs += '</div>';
     }
     document.getElementById("game-board").innerHTML = innerDivs;
+
+    // Reset state
     totals = [0, 0];
     gameOver = false;
-    document.getElementById("game-message").innerText = "It's " + players[whoseTurn] + "'s Turn";
+    whoseTurn = 0;
 
-    // show player names on scoreboard
+    // Update scoreboard
     document.getElementById("player1-name").innerText = players[0];
     document.getElementById("player2-name").innerText = players[1];
-}
-
-
-}
-
-function playGame(clickedDiv, divValue) 
-{
-	if (!gameOver)
-	{
-  //add x or o to playing field
-  clickedDiv.innerText = markers[whoseTurn];
-
-  //keep track of the players' totals for win combo
-  totals[whoseTurn] += divValue;
-
-  //call isWin() function
-  if (isWin())
-  {
-    document.getElementById("game-message").innerText = players[whoseTurn] + " wins!";
-	}
-	else if(gameOver)
-	{
-	document.getElementById("game-message").innerText = "House Wins Again Chump!";
-  } 
-  else 
-  {
-    //toggle player turn
-    if (whoseTurn == 1) { whoseTurn = 0; } else { whoseTurn = 1; }
-
-    //prevent clicking on same div again
-    clickedDiv.setAttribute("onclick", "");
-	
-
-    //update message to display next player
     document.getElementById("game-message").innerText = "It's " + players[whoseTurn] + "'s Turn";
-	}
-	if (isWin()) {
-    document.getElementById("game-message").innerText = players[whoseTurn] + " wins!";
-    wins[whoseTurn]++; // add a win
-    document.getElementById("player1-wins").innerText = wins[0];
-    document.getElementById("player2-wins").innerText = wins[1];
 }
 
+// Handle a move
+function playGame(clickedDiv, divValue) {
+    if (gameOver) return;
 
-  }
+    // Place marker
+    clickedDiv.innerText = markers[whoseTurn];
+    clickedDiv.setAttribute("onclick", ""); // prevent re-click
+    totals[whoseTurn] += divValue;
+
+    // Check win
+    if (isWin()) {
+        document.getElementById("game-message").innerText = players[whoseTurn] + " wins!";
+        wins[whoseTurn]++;
+        document.getElementById("player1-wins").innerText = wins[0];
+        document.getElementById("player2-wins").innerText = wins[1];
+        happySound.play();
+        return;
+    }
+
+    // Check tie
+    if (gameOver) {
+        document.getElementById("game-message").innerText = "It's a tie!";
+        ties++;
+        document.getElementById("ties").innerText = ties;
+        sadSound.play();
+        return;
+    }
+
+    // Switch turn
+    whoseTurn = (whoseTurn === 0 ? 1 : 0);
+    document.getElementById("game-message").innerText = "It's " + players[whoseTurn] + "'s Turn";
 }
 
-//win code logic
-function isWin()
- {
-  for (i = 0; i < winCodes.length; i++) 
-  {
-    if ((totals[whoseTurn] & winCodes[i]) == winCodes[i]) {gameOver = true; return true;}
-  }
-  
-  if (totals[0] + totals[1] == 511) {gameOver = true;}
+// Win/tie logic
+function isWin() {
+    for (var i = 0; i < winCodes.length; i++) {
+        if ((totals[whoseTurn] & winCodes[i]) === winCodes[i]) {
+            gameOver = true;
+            return true;
+        }
+    }
 
-  return false;
+    // Tie if all squares filled
+    if (totals[0] + totals[1] === 511) {
+        gameOver = true;
+    }
+
+    return false;
 }
-
